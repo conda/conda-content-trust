7c0b6ea7a5b68e6ac573739f31b7b854881b94e7
Ken Odegard <kodegard@anaconda.com>
2023-06-01 20:25:02 -0500

    Cleanup test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,59 +1,30 @@
 # Copyright (C) 2019 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-"""
-Integration tests for conda-content-trust/conda_content_trust/cli.py.
+import pytest
 
-Run the tests this way:
-    pytest tests/test_cli.py
-"""
-import subprocess
+from conda_content_trust.cli import cli
 
 
-def test_cli_basics():
-    assert not subprocess.call(["conda-content-trust", "-V"])
-    assert not subprocess.call(["conda-content-trust", "--version"])
-    assert not subprocess.call(["conda-content-trust", "--help"])
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["-V"],
+        ["--version"],
+        ["--help"],
+    ],
+)
+def test_cli_basic(args: list[str]):
+    with pytest.raises(SystemExit):
+        cli(args)
 
 
-def test_that_all_calls_complete():
-    assert not subprocess.call(
-        [
-            "conda-content-trust",
-            "verify-metadata",
-            "tests/testdata/1.root.json",
-            "tests/testdata/2.root.json",
-        ]
-    )
-    assert not subprocess.call(
-        [
-            "conda-content-trust",
-            "verify-metadata",
-            "tests/testdata/1.root.json",
-            "tests/testdata/key_mgr.json",
-        ]
-    )
-
-
-# def test_gpg_key_fingerprint():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
-
-# def test_():
-#   raise NotImplementedError()
+@pytest.mark.parametrize(
+    "trusted,untrusted",
+    [
+        ["tests/testdata/1.root.json", "tests/testdata/2.root.json"],
+        ["tests/testdata/1.root.json", "tests/testdata/key_mgr.json"],
+    ],
+)
+def test_cli_verify_metadata(trusted: str, untrusted: str):
+    err = cli(["verify-metadata", trusted, untrusted])
+    assert not err
