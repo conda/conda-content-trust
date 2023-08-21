a9c06220c39517727d86b0be56a3bf023d8f00bd
Daniel Holth <dholth@anaconda.com>
2023-08-21 12:28:03 -0400

    use importorskip

--- tests/test_subcommand.py
+++ tests/test_subcommand.py
@@ -1,8 +1,11 @@
-from conda_content_trust.plugin import conda_subcommands
+import pytest
 
 
 def test_get_subcommand():
     """
     Run subcommand generator code for coverage.
     """
+    conda_subcommands = pytest.importorskip(
+        "conda_condent_trust.plugin.conda_subcommands"
+    )
     assert len(list(conda_subcommands())) == 1
