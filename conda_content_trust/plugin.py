7cfb2edaf10e413609c8d91dd0bffaeef6ddf41c
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-12-16 10:41:28 +0530

    Make it a versioned import

--- conda_content_trust/plugin.py
+++ conda_content_trust/plugin.py
@@ -1,13 +1,23 @@
 # Copyright (C) 2019 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-import conda.plugins.types
+
+from conda import __version__ as conda_version
 
 from .cli import cli
+
+version_parts = conda_version.split('.')
+major = int(version_parts[0]) # year
+minor = int(version_parts[1]) if len(version_parts) > 1 else 0 # month
+
+if (major, minor) >= (25, 9):
+    from conda.plugins.types import CondaSubcommand
+else:
+    from conda.plugins import CondaSubcommand
 
 
 @conda.plugins.hookimpl
 def conda_subcommands():
-    yield conda.plugins.types.CondaSubcommand(
+    yield CondaSubcommand(
         name="content-trust",
         summary="Signing and verification tools for Conda",
         action=cli,
