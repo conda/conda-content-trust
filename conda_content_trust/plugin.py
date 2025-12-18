099addf8aad86a8adc8d64f5953ca371a2d98df7
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-12-19 00:17:24 +0530

    Import `CondaSubcommand` from `conda.plugins.types` (#238)
    * Remove extraneous colon
    
    * Import `CondaSubcommand` from `conda.plugins.types`

--- conda_content_trust/plugin.py
+++ conda_content_trust/plugin.py
@@ -1,13 +1,13 @@
 # Copyright (C) 2019 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-import conda.plugins
+import conda.plugins.types
 
 from .cli import cli
 
 
 @conda.plugins.hookimpl
 def conda_subcommands():
-    yield conda.plugins.CondaSubcommand(
+    yield conda.plugins.types.CondaSubcommand(
         name="content-trust",
         summary="Signing and verification tools for Conda",
         action=cli,
