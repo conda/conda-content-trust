148320032715abb8657e190950c9002a21410d4f
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-12-19 00:09:46 +0530

    Drop versioned check

--- conda_content_trust/plugin.py
+++ conda_content_trust/plugin.py
@@ -1,23 +1,13 @@
 # Copyright (C) 2019 Anaconda, Inc
 # SPDX-License-Identifier: BSD-3-Clause
-
-from conda import __version__ as conda_version
+import conda.plugins.types
 
 from .cli import cli
-
-version_parts = conda_version.split(".")
-major = int(version_parts[0])  # year
-minor = int(version_parts[1]) if len(version_parts) > 1 else 0  # month
-
-if (major, minor) >= (25, 9):
-    from conda.plugins.types import CondaSubcommand
-else:
-    from conda.plugins import CondaSubcommand
 
 
 @conda.plugins.hookimpl
 def conda_subcommands():
-    yield CondaSubcommand(
+    yield conda.plugins.types.CondaSubcommand(
         name="content-trust",
         summary="Signing and verification tools for Conda",
         action=cli,
